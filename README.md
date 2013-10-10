105e47c56d9b57c8fea2a7d9543ecd3f17a3bedc
grturner <dafiremaster@gmail.com>
2013-10-10 14:41:35 -0400

    Commit 02
    Cleaned up some of the application calls, added cheesy artwork.

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-cwflauncher
+Fourth Party Launcher
 ===========
-Meaningful text will go here.
+Welcome to the Fourth Party Launcher for Cube World. 
+
+Instructions:
+Download launcher.exe and run it. 
+The program does not need to be placed into your Cube World install directory. 
+Note: As of right now, this launcher will only work with the default install path. If you have moved your Cube World folder elsewhere, this will not work for you. 
+
+Support:
+For support questions, comments, feedback please refer to our page on CubeWorldForum.org
+http://cubeworldforum.org/topic/15159-fourth-party-launcher/
+
+Contributors:
+Gilbert Turner aka Tech Pony
+Aristaeus
+Obsoletepotato
--- foruthpartylauncher/WindowsFormsApplication1/LauncherMain.Designer.cs
+++ foruthpartylauncher/WindowsFormsApplication1/LauncherMain.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.Launch = new System.Windows.Forms.Button();
             this.CubeWorldForum = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.Skins = new System.Windows.Forms.Button();
@@ -37,20 +37,20 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.ServerSettings = new System.Windows.Forms.Button();
+            this.LaunchServer = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // Launch
             // 
-            this.button1.Location = new System.Drawing.Point(50, 246);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(351, 33);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Launch Cube World";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Launch_Click);
+            this.Launch.Location = new System.Drawing.Point(50, 246);
+            this.Launch.Name = "Launch";
+            this.Launch.Size = new System.Drawing.Size(351, 33);
+            this.Launch.TabIndex = 0;
+            this.Launch.Text = "Launch Cube World";
+            this.Launch.UseVisualStyleBackColor = true;
+            this.Launch.Click += new System.EventHandler(this.Launch_Click);
             // 
             // CubeWorldForum
             // 
@@ -60,10 +60,11 @@
             this.CubeWorldForum.TabIndex = 1;
             this.CubeWorldForum.Text = "Cube World Forum";
             this.CubeWorldForum.UseVisualStyleBackColor = true;
-            this.CubeWorldForum.Click += new System.EventHandler(this.button2_Click);
+            this.CubeWorldForum.Click += new System.EventHandler(this.CubeWorldForum_Click);
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Image = global::WindowsFormsApplication1.Properties.Resources.ivthparty;
             this.pictureBox1.Location = new System.Drawing.Point(7, 14);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(440, 130);
@@ -78,7 +79,7 @@
             this.Skins.TabIndex = 4;
             this.Skins.Text = "Skins";
             this.Skins.UseVisualStyleBackColor = true;
-            this.Skins.Click += new System.EventHandler(this.button4_Click);
+            this.Skins.Click += new System.EventHandler(this.Skins_Click);
             // 
             // OrigLauncher
             // 
@@ -88,7 +89,7 @@
             this.OrigLauncher.TabIndex = 5;
             this.OrigLauncher.Text = "Run Original Launcher";
             this.OrigLauncher.UseVisualStyleBackColor = true;
-            this.OrigLauncher.Click += new System.EventHandler(this.button5_Click);
+            this.OrigLauncher.Click += new System.EventHandler(this.OrigLauncher_Click);
             // 
             // GameSettings
             // 
@@ -98,7 +99,7 @@
             this.GameSettings.TabIndex = 6;
             this.GameSettings.Text = "Game Settings";
             this.GameSettings.UseVisualStyleBackColor = true;
-            this.GameSettings.Click += new System.EventHandler(this.button6_Click);
+            this.GameSettings.Click += new System.EventHandler(this.GameSettings_Click);
             // 
             // textBox1
             // 
@@ -110,7 +111,6 @@
             this.textBox1.TabIndex = 7;
             this.textBox1.Text = "Fourth Party Launcher";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
@@ -133,33 +133,33 @@
             this.textBox3.Text = "Version 0.001a";
             this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // button2
+            // ServerSettings
             // 
-            this.button2.Location = new System.Drawing.Point(50, 398);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(169, 50);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "Server Settings";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            this.ServerSettings.Location = new System.Drawing.Point(50, 398);
+            this.ServerSettings.Name = "ServerSettings";
+            this.ServerSettings.Size = new System.Drawing.Size(169, 50);
+            this.ServerSettings.TabIndex = 10;
+            this.ServerSettings.Text = "Server Settings";
+            this.ServerSettings.UseVisualStyleBackColor = true;
+            this.ServerSettings.Click += new System.EventHandler(this.ServerSettings_Click);
             // 
-            // button3
+            // LaunchServer
             // 
-            this.button3.Location = new System.Drawing.Point(232, 398);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(169, 50);
-            this.button3.TabIndex = 11;
-            this.button3.Text = "Launch Server";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.LaunchServer.Location = new System.Drawing.Point(232, 398);
+            this.LaunchServer.Name = "LaunchServer";
+            this.LaunchServer.Size = new System.Drawing.Size(169, 50);
+            this.LaunchServer.TabIndex = 11;
+            this.LaunchServer.Text = "Launch Server";
+            this.LaunchServer.UseVisualStyleBackColor = true;
+            this.LaunchServer.Click += new System.EventHandler(this.ServerLaunch_Click);
             // 
             // LauncherMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(456, 472);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.LaunchServer);
+            this.Controls.Add(this.ServerSettings);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -168,10 +168,10 @@
             this.Controls.Add(this.Skins);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.CubeWorldForum);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Launch);
             this.Name = "LauncherMain";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "IVth Party Launcher";
+            //this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -180,7 +180,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Launch;
         private System.Windows.Forms.Button CubeWorldForum;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button Skins;
@@ -189,8 +189,8 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button GameSettings;
         private System.Windows.Forms.Button OrigLauncher;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button ServerSettings;
+        private System.Windows.Forms.Button LaunchServer;
     }
 }
 
